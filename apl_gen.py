b0c3a9a3cfb4a220dd85ca1cd4197310b72df697
Frank Müller <frmimue@gmail.com>
2018-12-30 01:40:36 +0100

    Optimized condition structure

--- apl_gen.py
+++ apl_gen.py
@@ -3,22 +3,46 @@ import json
 import subprocess
 from operator import itemgetter
 
+class IntCondition:
+        def __init__(self, condition, range):
+                self.condition = condition
+                self.range = range
+
+        def generate(self):
+                return np.random.choice(["", ",if=" + np.random.choice(["", "!"]) + self.condition + np.random.choice(["<", ">", "=", ">=", "<="]) + str(np.random.choice(self.range))])
+
+class Conditions:
+        def __init__(self):
+                self.conditions = []
+
+        def generate(self):
+                return np.random.choice(self.conditions).generate()
+
+        def add(self, condition):
+                self.conditions.append(condition)
+
 actions = np.array(["use_items", "potion", "call_dreadstalkers", "summon_demonic_tyrant", "hand_of_guldan", "demonbolt", "shadow_bolt", "grimoire_felguard", "summon_vilefiend", "bilescourge_bombers"], dtype=np.object)
 
-conditions = np.array(["", "soul_shard>=3"], dtype=np.object)
+conditions = Conditions()
+conditions.add(IntCondition("soul_shard", range(0,5)))
+conditions.add(IntCondition("buff.demonic_core.stack", range(0, 4)))
 
 chromosomes = []
 
-for i in range(1):
+for i in range(2):
 
         print("Iteration: " + str(i))
 
-        for i in range(100-len(chromosomes)):
-                chromosomes.append({"Genes" : np.random.choice(actions + ',if=' + np.random.choice(conditions, len(actions)), len(actions), False)})
+        for i in range(1000-len(chromosomes)):
+                genes = []
+                for j in range(np.random.randint(len(actions), 30)):
+                        genes.append(np.random.choice(actions) + conditions.generate())
+                chromosomes.append({"Genes" : genes})
+
         file = open(r"tmp\apl_gen_run.simc", "w")
         file.write("apl_gen_base.simc\n")
 
-        for i in range(100):
+        for i in range(1000):
                 file.write("profileset.\"" + str(i) + "\"=\"actions=/" + chromosomes[i]["Genes"][0] + "\n")
                 for j in range(1, len(chromosomes[i]["Genes"])):
                         file.write("profileset.\"" + str(i) + "\"+=\"actions+=/" + chromosomes[i]["Genes"][j] + "\n")
@@ -42,7 +66,6 @@ for i in range(1):
         json_data.close()
 
         chromosomes = sorted(chromosomes, key=itemgetter("DPS"), reverse=True)
-        chromosomes = chromosomes[:10]
+        chromosomes = chromosomes[:(len(chromosomes)//10)]
 
-        for chromosome in chromosomes:
-                print(chromosome)
+print(chromosomes[0])
